53ef9b312e54cc89dab70a3bea26e161c403c6f7
Sara-226 <yidjabazie@gmail.com>
2021-03-11 12:58:52 +0100

    comit2

---
+++ afterwork/database/migrations/2021_03_11_110552_create_utilisateurs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilisateursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->string('nom', 50);
+            $table->string('prenom', 50);
+            $table->string('mail', 50);
+            $table->string('password', 50);
+            $table->string('role', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+}
